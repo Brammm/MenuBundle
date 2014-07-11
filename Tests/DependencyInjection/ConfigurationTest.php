b25e32bd00803806ca5c1e763dddbfc7c0b4ed7d
Bram Van der Sype <bram.vandersype@gmail.com>
2014-07-11 14:14:02 +0200

    Added configuration test

---
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use Brammm\MenuBundle\DependencyInjection\Configuration;
+use Symfony\Component\Config\Definition\Processor;
+
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHasDefaultTheme()
+    {
+        $config = [];
+
+        $config = $this->process($config);
+
+        $this->assertNotEmpty($config['theme']);
+    }
+
+    public function testCanOverrideTheme()
+    {
+        $config = [
+            ['theme' => 'foo']
+        ];
+
+        $config = $this->process($config);
+
+        $this->assertEquals('foo', $config['theme']);
+    }
+
+    /**
+     * Processes an array of configurations and returns a compiled version.
+     *
+     * @param array $configs An array of raw configurations
+     *
+     * @return array A normalized array
+     */
+    protected function process($configs)
+    {
+        $processor = new Processor();
+
+        return $processor->processConfiguration(new Configuration(), $configs);
+    }
+} 
