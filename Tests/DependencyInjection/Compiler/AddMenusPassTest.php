ceea91d86bb8c5b425a1874405fce05bd3df6dd9
Bram Van der Sype <bram.vandersype@gmail.com>
2014-07-11 14:01:27 +0200

    refactored test

--- Tests/DependencyInjection/Compiler/AddMenusPassTest.php
+++ Tests/DependencyInjection/Compiler/AddMenusPassTest.php
@@ -9,29 +9,36 @@ class AddMenusPassTest extends \PHPUnit_Framework_TestCase
 {
     public function testTaggedMenusGetAddedToProvider()
     {
-        $services = array(
-            'menu_service' => array(),
-        );
+        $container = $this->getContainerMock();
 
+        $addCacheWarmerPass = new AddMenusPass();
+        $addCacheWarmerPass->process($container);
+    }
+
+    private function getDefinitionMock()
+    {
         $definition = $this->getMock('Symfony\Component\DependencyInjection\Definition');
-        $container = $this->getMock('Symfony\Component\DependencyInjection\ContainerBuilder');
-
-        $container->expects($this->atLeastOnce())
-            ->method('findTaggedServiceIds')
-            ->with('brammm_menu.menu')
-            ->will($this->returnValue($services));
-        $container->expects($this->atLeastOnce())
-            ->method('getDefinition')
-            ->with('brammm_menu.menu_provider')
-            ->will($this->returnValue($definition));
-
         $definition->expects($this->once())
             ->method('addMethodCall')
             ->with('addMenu', [
                 new Reference('menu_service'),
             ]);
+        return $definition;
+    }
 
-        $addCacheWarmerPass = new AddMenusPass();
-        $addCacheWarmerPass->process($container);
+    private function getContainerMock()
+    {
+        $container = $this->getMock('Symfony\Component\DependencyInjection\ContainerBuilder');
+
+        $container->expects($this->atLeastOnce())
+            ->method('findTaggedServiceIds')
+            ->with('brammm_menu.menu')
+            ->will($this->returnValue(['menu_service' => []]));
+        $container->expects($this->atLeastOnce())
+            ->method('getDefinition')
+            ->with('brammm_menu.menu_provider')
+            ->will($this->returnValue($this->getDefinitionMock()));
+
+        return $container;
     }
 }
