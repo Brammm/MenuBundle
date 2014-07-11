ba1768d7a16a9390629188afd1b3b4f306d4055c
Bram Van der Sype <bram.vandersype@gmail.com>
2014-07-11 15:26:33 +0200

    Added test for TwigRenderer

---
+++ Tests/Renderer/TwigRendererTest.php
@@ -0,0 +1,153 @@
+<?php
+
+use Brammm\MenuBundle\Renderer\TwigRenderer;
+
+class TwigRendererTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var TwigRenderer */
+    private $SUT;
+    /** @var \Twig_Environment|\PHPUnit_Framework_MockObject_MockObject */
+    private $environment;
+    /** @var \Twig_TemplateTest|\PHPUnit_Framework_MockObject_MockObject */
+    private $template;
+
+    public function setUp()
+    {
+        $this->environment = $this->getMockBuilder('\Twig_Environment')
+            ->getMock();
+
+        $this->template = $this->getMockBuilder('Twig_TemplateTest')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->SUT = new TwigRenderer($this->environment, 'foo');
+    }
+
+    public function testCanRenderMenu()
+    {
+        $this->environmentWillLoadTheme('foo');
+        $this->templateRendersBlock('menu');
+
+        $this->SUT->renderMenu($this->getItem('item'));
+    }
+
+    public function testCanRenderMenuItem()
+    {
+        $this->environmentWillLoadTheme('foo');
+        $this->templateRendersBlock('menu_item');
+
+        $this->SUT->renderItem($this->getItem('item'));
+    }
+
+    public function testCanSetTheme()
+    {
+        $item = $this->getItem('item');
+
+        $this->SUT->setTheme($item, 'bar');
+
+        $this->environmentWillLoadTheme('bar');
+
+        $this->SUT->renderMenu($item);
+    }
+
+    public function testCanRenderParentTheme()
+    {
+        $parent = $this->getItem('parent');
+        $child  = $this->getItem('child');
+        $child->expects($this->once())
+            ->method('getParent')
+            ->will($this->returnValue($parent));
+
+        $this->SUT->setTheme($parent, 'baz');
+
+        $this->environmentWillLoadTheme('baz');
+
+        $this->SUT->renderMenu($child);
+    }
+
+    private function environmentWillLoadTheme($theme)
+    {
+        $this->environment->expects($this->once())
+            ->method('loadTemplate')
+            ->with($theme)
+            ->will($this->returnValue($this->template));
+    }
+
+    private function templateRendersBlock($block)
+    {
+        $this->template->expects($this->once())
+            ->method('renderBlock')
+            ->with($block);
+    }
+
+    private function getItem($name)
+    {
+        $item = $this->getMockBuilder('Brammm\MenuBundle\Menu\MenuItem')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $item->expects($this->any())
+            ->method('getName')
+            ->will($this->returnValue($name));
+
+        return $item;
+    }
+}
+
+class Twig_TemplateTest extends Twig_Template
+{
+    protected $useExtGetAttribute = false;
+
+    public function __construct(Twig_Environment $env, $useExtGetAttribute = false)
+    {
+        parent::__construct($env);
+        $this->useExtGetAttribute = $useExtGetAttribute;
+        Twig_Template::clearCache();
+    }
+
+    public function getZero()
+    {
+        return 0;
+    }
+
+    public function getEmpty()
+    {
+        return '';
+    }
+
+    public function getString()
+    {
+        return 'some_string';
+    }
+
+    public function getTrue()
+    {
+        return true;
+    }
+
+    public function getTemplateName()
+    {
+    }
+
+    public function getDebugInfo()
+    {
+        return array();
+    }
+
+    protected function doGetParent(array $context)
+    {
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+    }
+
+    public function getAttribute($object, $item, array $arguments = array(), $type = Twig_Template::ANY_CALL, $isDefinedTest = false, $ignoreStrictCheck = false)
+    {
+        if ($this->useExtGetAttribute) {
+            return twig_template_get_attributes($this, $object, $item, $arguments, $type, $isDefinedTest, $ignoreStrictCheck);
+        } else {
+            return parent::getAttribute($object, $item, $arguments, $type, $isDefinedTest, $ignoreStrictCheck);
+        }
+    }
+}
